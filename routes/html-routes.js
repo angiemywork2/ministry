7fb9a0be1711b78a2b3391b40871a2253edd176b
EliseoMejia <eliseofmejia@gmail.com>
2017-10-14 20:55:47 -0400

    changes to handlebars and public js files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,12 +2,14 @@
 //Dependencies
 var path = require('path');
 
-var Blog = require("../models/blog.js")
+var Blog = require("../models/index.js")
 //Routes
 module.exports = function(app){
+	//that hande the static html pages
+	//examples
+	// app.get("/", function(req, res) {
+	// res.sendFile(path.join(__dirname, "../public/blog.html"));
+	// });
 
-	//routes that render handlebars pages
-	app.get("/blog", function(req,res){
-		render.sendFile(path.join(__dirname, "../public/"))
-	})
+
 };
