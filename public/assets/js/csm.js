b02b0de22b8607a2b88f831d1c85e54a6cb4be71
EliseoMejia <eliseofmejia@gmail.com>
2017-10-14 21:00:59 -0400

    commit of changes to public

--- public/assets/js/csm.js
+++ public/assets/js/csm.js
@@ -1,1 +1,20 @@
 console.log("connected")
+
+$(document).ready(function() {
+	//optional query string from url
+	var url = window.location.search;
+	var blogId;
+	//flag to see if post is being updated or not
+	var updating = false;
+	//pulling blog id from url if it exists
+	if (url.indexOf("?blog_id=") !== -1) {
+		blogId = url.split("=")[1];
+		getBlogData(postId);
+	}
+	//post references
+	var titleInput= $("#postTitle");
+	var bodyIndput= $("#postBody")
+
+	//event listener when submit button is clicked
+	$()
+})
--- public/assets/js/postBlog.js
+++ public/assets/js/postBlog.js
@@ -1,3 +1,11 @@
  $(function() {
  	
+ 	var titleInput = $("#blogTitle");
+ 	var bodyInput = $("#blogText");
+
+
+ 	$("#postBlog").on("click", function(event) {
+ 		var id = $(this).data("id");
+
+ 	})
  })
